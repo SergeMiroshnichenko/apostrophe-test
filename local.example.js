07b0365dcaa16d0ca5791cfc3d62c3cd7fd42c49
SergeMiroshnichenko <serge.m@speedandfunction.com>
2020-12-01 18:14:20 +0200

    Added simple navigation

--- local.example.js
+++ local.example.js
@@ -12,10 +12,10 @@ module.exports = {
     // If these are your db settings then you don't need to be explicit. If not
     // you can uncomment this and get more specific.
     'apostrophe-db': {
-      // uri: 'mongodb://localhost:27017/apostrophe-sandbox'
+      uri: 'mongodb://localhost:27017/apostrophe-sandbox',
       // There is legacy support for host, port, name, user and password options,
       // but this is not necessary. They can all go in the uri option like this:
-      // mongodb://user:password@host:port/dbname
+      mongodb: 'user:password@host:port/dbname'
     }
   }
 };
